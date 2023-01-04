6e20abf4e717d1b39827e342f2ddde4c803f7306
Sam <samuel.fitz@btinternet.com>
2023-01-04 21:03:58 +0000

    fix: website reset

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-  images: {
-    dangerouslyAllowSVG: true,
-    domains: ["images.unsplash.com", "thrangra.sirv.com"],
-  },
-  reactStrictMode: true,
-  swcMinify: true,
 };
-
-module.exports = nextConfig;
---
+++ nmfc/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '5v6ky3zx',
+    dataset: 'production'
+  }
+})
