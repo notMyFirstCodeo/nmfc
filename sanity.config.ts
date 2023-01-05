0c0f8aee5dfc32867d784ec054ac17aea8896bef
Sam <samuel.fitz@btinternet.com>
2023-01-05 14:39:53 +0000

    feat: sanity studio client side

---
+++ sanity.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
+export default defineConfig({
+  basePath: "/studio",
+  name: "default",
+  title: "Not My First Codeo Studio",
+  projectId,
+  dataset,
+  // Desk tool allows us to update content
+  // Vision tool allows us to test GROQ queries
+  plugins: [deskTool(), visionTool()],
+  schema: {
+    types: schemaTypes,
+  },
+});
