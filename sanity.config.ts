d6c50a206f290517c113d00e2452dda64848b5e8
Sam <samuel.fitz@btinternet.com>
2023-01-05 16:06:11 +0000

    fix: modify project key and sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,8 +8,8 @@ const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
   basePath: "/studio",
-  name: "default",
-  title: "Not My First Codeo Studio",
+  name: "nmfc-content-studio",
+  title: "nMFC",
   projectId,
   dataset,
   // Desk tool allows us to update content
